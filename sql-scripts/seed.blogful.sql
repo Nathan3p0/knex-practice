7d550a76c541c0fa94fec09286ceec180dd7c7d7
Nathan Szelag <nszelag@live.com>
2019-08-15 18:43:29 -0400

    created blogful db and seeded. Wrote failing tests

---
+++ sql-scripts/seed.blogful.sql
@@ -0,0 +1,23 @@
+INSERT INTO blogful_articles (title, date_published, content)
+VALUES
+    ('Intro to HTML', now() - '21 days'::INTERVAL, 'Article content here......'),
+    ('Why Semantic HTML is important', now() - '20 days'::INTERVAL, 'Article content here......'),
+    ('JavaScript the basics', now() - '19 days'::INTERVAL, 'Article content here......'),
+    ('How to create a node server', now() - '18 days'::INTERVAL, 'Article content here......'),
+    ('Express for beginners', now() - '17 days'::INTERVAL, 'Article content here......'),
+    ('SQL queries you need to know', now() - '16 days'::INTERVAL, 'Article content here......'),
+    ('CSS selectors', now() - '15 days'::INTERVAL, 'Article content here......'),
+    ('Why won''t this thing center?', now() - '14 days'::INTERVAL, 'Article content here......'),
+    ('Time to learn React', now() - '13 days'::INTERVAL, 'Article content here......'),
+    ('Taken out of Context', now() - '12 days'::INTERVAL, 'Article content here......'),
+    ('Modularizing your code', now() - '11 days'::INTERVAL, 'Article content here......'),
+    ('Intro to testing React Components', now() - '10 days'::INTERVAL, 'Article content here......'),
+    ('Knex, it isn''t just a toy', now() - '9 days'::INTERVAL, 'Article content here......'),
+    ('Deploying a server to Heroku', now() - '8 days'::INTERVAL, 'Article content here......'),
+    ('Headless CMS', now() - '7 days'::INTERVAL, 'Article content here......'),
+    ('HTTP request and response cycle', now() - '6 days'::INTERVAL, 'Article content here......'),
+    ('Keeping your Javascript Secure', now() - '5 days'::INTERVAL, 'Article content here......'),
+    ('Templating Engines in Express', now() - '4 days'::INTERVAL, 'Article content here......'),
+    ('Keep your code DRY', now() - '3 days'::INTERVAL, 'Article content here......'),
+    ('Writing code for kids', now() - '2 days'::INTERVAL, 'Article content here......')
+    ;
---
+++ test/articles-service.spec.js
@@ -0,0 +1,50 @@
+const ArticlesService = require('../src/articles-service');
+const knex = require('knex');
+
+describe(`Articles service object`, () => {
+    let db;
+    let testArticles = [{
+        id: 1,
+        date_published: new Date(),
+        title: 'First test post!',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?'
+    },
+    {
+        id: 2,
+        date_published: new Date(),
+        title: 'Second test post!',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?'
+    },
+    {
+        id: 3,
+        date_published: new Date(),
+        title: 'Third test post!',
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?'
+    }
+    ]
+    before(() => {
+        db = knex({
+            client: 'pg',
+            connection: process.env.TEST_DB_URL
+        })
+    });
+
+    before(() => {
+        return db
+            .into('blogful_articles')
+            .insert(testArticles)
+    })
+
+    before(() => db('blogful_articles').truncate());
+
+    after(() => db.destroy());
+
+    describe(`getAllArticles()`, () => {
+        it(`resolves all articles from 'blogful_articles' table`, () => {
+            return ArticlesService.getAllArticles(db)
+                .then(actual => {
+                    expect(actual).to.eql(testArticles);
+                })
+        })
+    })
+});
